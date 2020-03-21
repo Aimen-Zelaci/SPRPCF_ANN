3e3a9d4202670f382aea80db980f76e8c93aaf60
ironman <insideminder@yahoo.com>
2020-03-21 16:29:46 +0200

    updata: full-batch-tr

--- src/tf_ann.py
+++ src/tf_ann.py
@@ -15,13 +15,16 @@ print(device_lib.list_local_devices())
 # Load data
 tr_data, tr_labels, va_data, va_labels, test_data, test_labels = data_handler.load_data(fname='.\data\data.xlsx')
 
+
 # Augment
 
-#tr_data, tr_labels = data_handler.augment_data(tr_data, tr_labels, 1000, fname='.\gen_data\gen_data.txt')
+for _ in [1000]:
+    tr_data, tr_labels = data_handler.augment_data(tr_data, tr_labels, _, fname='.\gen_data\gen_data.txt')
 
+print(tr_labels[0])
 # Parameters
-learning_rate = 0.0001
-training_epochs = 200
+learning_rate = 0.001
+training_epochs = 2000
 batch_size = 8
 
 # Network Parameters
@@ -36,38 +39,35 @@ weight_initializer = tf.variance_scaling_initializer(mode="fan_avg", distributio
 bias_initializer = tf.zeros_initializer()
 
 
-def initializer():
-    weights = {
-        'h1': tf.Variable(weight_initializer([n_input, n_hidden])),
-        'out': tf.Variable(weight_initializer([n_hidden, n_outputs]))
-    }
-    biases = {
-        'b1': tf.Variable(bias_initializer([n_hidden])),
-        'out': tf.Variable(bias_initializer([n_outputs]))
-    }
+weights = {
+    'h1': tf.Variable(weight_initializer([n_input, n_hidden])),
+    'out': tf.Variable(weight_initializer([n_hidden, n_outputs]))
+}
+biases = {
+    'b1': tf.Variable(bias_initializer([n_hidden])),
+    'out': tf.Variable(bias_initializer([n_outputs]))
+}
 
-    # Create hidden layers
-    for i in range(1,num_hidden_layers):
-        weights['h{}'.format(i+1)] = tf.Variable(weight_initializer([n_hidden, n_hidden]))
-        biases['b{}'.format(i+1)] = tf.Variable(bias_initializer([n_outputs]))
+# Create hidden layers
+for i in range(1,num_hidden_layers):
+    weights['h{}'.format(i+1)] = tf.Variable(weight_initializer([n_hidden, n_hidden]))
+    biases['b{}'.format(i+1)] = tf.Variable(bias_initializer([n_outputs]))
 
-    return [weights, biases]
-
-# We are going to use Batch Normalization, therefore we don't need the biases
-weights, _ = initializer()
 
 # Create model
-def multilayer_perceptron(x, weights, biases):
-    in_x = tf.matmul(x, weights['h1'])
+def multilayer_perceptron(x):
+    in_x = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
     in_x = tf.nn.relu(in_x)
-    in_x = tf.layers.batch_normalization(in_x)
+    #in_x = tf.nn.dropout(in_x, keep_prob = 0.8)
+    #in_x = tf.layers.batch_normalization(in_x)
 
     for i in range(1,num_hidden_layers):
         in_x = tf.add(tf.matmul(in_x, weights['h{}'.format(i+1)]), biases['b{}'.format(i+1)])
         in_x = tf.nn.relu(in_x)
-        in_x = tf.layers.batch_normalization(in_x)
+        #in_x = tf.nn.dropout(in_x, keep_prob = 0.5)
+        #in_x = tf.layers.batch_normalization(in_x)
 
-    out_layer = tf.matmul(in_x, weights['out'])
+    out_layer = tf.add(tf.matmul(in_x, weights['out']), biases['out'])
     out_layer = tf.nn.relu(out_layer)
 
     return out_layer
@@ -75,8 +75,9 @@ def multilayer_perceptron(x, weights, biases):
 # Initializing the variables
 X = tf.placeholder("float", [None, n_input], name='X')
 Y = tf.placeholder("float", [None, n_outputs], name='Y')
+keep_prob = tf.placeholder(tf.float32)
 
-out = multilayer_perceptron(X, weights, _)
+out = multilayer_perceptron(X)
 
 # Define loss and optimizer
 loss_op = tf.losses.mean_squared_error(Y, out)
@@ -95,23 +96,26 @@ def train():
     for epoch in range(training_epochs):
         start_epoch = time.time()
         avg = 0.
-
-        shuffle_indices = sp.random.permutation(sp.arange(len(tr_labels)))
-        X_train = tr_data[shuffle_indices]
-        y_train = tr_labels[shuffle_indices]
         # Minibatch training
+        '''
         for i in range(0, len(y_train) // batch_size):
             start = i * batch_size
             batch_x = X_train[start:start + batch_size]
             batch_y = y_train[start:start + batch_size]
             # Run optimizer with batch
-            sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
-            cost = sess.run(loss_op, feed_dict={X:batch_x, Y:batch_y})
-            avg+=cost
+            _, cost = sess.run([train_op, loss_op], feed_dict={X: tr_data, Y: tr_labels, keep_prob:0.5})
+        '''
+        _, cost = sess.run([train_op, loss_op], feed_dict={X: tr_data, Y: tr_labels, keep_prob:0.5})
 
         va_mse = sess.run(loss_op, feed_dict={X:va_data, Y:va_labels})
-        print('tr_loss at EPOCH {} is {} \t\t va_MSE is {} \t\t {} sec'.format(epoch+1, avg / (len(y_train) // batch_size), va_mse, time.time() - start_epoch ))
 
+        print('tr_loss at EPOCH {} is {:.6f} \t\t va_MSE is {:.6f} \t\t {:.6f} sec'.format(epoch+1, cost, va_mse, time.time() - start_epoch ))
+
+    pred = sess.run(out, feed_dict={X:va_data})
+    plt.scatter(va_labels, pred)
+    plt.plot(va_labels, va_labels)
+    plt.grid()
+    plt.show()
 train()
 
 print("Training Finished! {} sec".format( time.time()- start))
